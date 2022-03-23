2702943b5631d039a5394d9233abc6ea0ad42be6
DESKTOP-NS1P2R3\joses <josemaria@nubimetrics.com>
2022-03-23 09:49:35 -0300

    dropdown installed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import DropdownTreeSelect from 'react-dropdown-tree-select'
+import 'react-dropdown-tree-select/dist/styles.css'
+
+
+const data = {
+  label: 'search me',
+  value: 'searchme',
+  children: [
+    {
+      label: 'search me too',
+      value: 'searchmetoo',
+      children: [
+        {
+          label: 'No one can get me',
+          value: 'anonymous',
+        },
+      ],
+    },
+  ],
+}
+
+const onChange = (currentNode, selectedNodes) => {
+  console.log('onChange::', currentNode, selectedNodes)
+}
+const onAction = (node, action) => {
+  console.log('onAction::', action, node)
+}
+const onNodeToggle = currentNode => {
+  console.log('onNodeToggle::', currentNode)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <DropdownTreeSelect data={data} onChange={onChange} onAction={onAction} onNodeToggle={onNodeToggle} />
     </div>
   );
 }
